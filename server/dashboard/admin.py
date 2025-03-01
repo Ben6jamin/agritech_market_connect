fd97443ce47b24309d0b062518c58ef0693a93ef
Ben6jamin <b.ishimwe1@alustudent.com>
2025-03-01 15:53:44 +0200

    add feedbacks in admin

--- server/dashboard/admin.py
+++ server/dashboard/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Seed, Fertilizer, MarketPrice, WeatherForecast, FarmingTip
+from .models import Seed, Fertilizer, MarketPrice, WeatherForecast, FarmingTip, FeedBack
 
 admin.site.register(Seed)
 admin.site.register(Fertilizer)
 admin.site.register(MarketPrice)
 admin.site.register(WeatherForecast)
 admin.site.register(FarmingTip)
+admin.site.register(FeedBack)
